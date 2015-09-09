05f32fce7c3532fa6afeb0da946aa83e882760f4
Norman Paniagua <normanpaniagua@gmail.com>
2015-09-09 17:33:23 -0400

    Enhanced configuration environment support

--- config/server.js
+++ config/server.js
@@ -1,7 +1,20 @@
-module.exports = {
-	port: 3000,
-	publicPath: process.cwd() + '/public',
-	upload: {
-		uploadDir: process.cwd() + '/tmp',
-	}	
-};
+var options = {
+	development: {
+		port: 3000,
+		publicPath: process.cwd() + '/public',
+		upload: {
+			uploadDir: process.cwd() + '/tmp',
+			multipart: false
+		}	
+	},
+	production: {
+		port: 80,
+		publicPath: process.cwd() + '/public',
+		upload: {
+			uploadDir: process.cwd() + '/tmp',
+			multipart: false
+		}	
+	}
+}
+
+module.exports = options[process.env.environment] || options.production;
